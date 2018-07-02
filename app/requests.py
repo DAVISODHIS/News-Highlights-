86cd445a5ef08dc1badf725c0854fa37c179529a
maxwellharon <maxwellharon@gmail.com>
2018-07-02 15:22:04 +0300

    Add get articles function

--- app/requests.py
+++ app/requests.py
@@ -9,22 +9,22 @@ api_key = app.config['NEWS_API_KEY']
 base_url = app.config["NEWS_API_BASE_URL"]
 
 
-def get_articles(endpoint, category):
-    """ function that gets the headlines on request """
-
-    get_articles_url = base_url.format(endpoint, category, '', api_key)
-
-    with urllib.request.urlopen(get_articles_url) as url:
-        get_articles_data = url.read()
-        get_articles_response = json.loads(get_articles_data)
-
-        articles_results = None
-
-        if get_articles_response['articles']:
-            articles_results_list = get_articles_response['articles']
-            articles_results = process_results(articles_results_list)
-
-    return articles_results
+# def get_articles(endpoint, category):
+#     """ function that gets the headlines on request """
+#
+#     get_articles_url = base_url.format(endpoint, category, '', api_key)
+#
+#     with urllib.request.urlopen(get_articles_url) as url:
+#         get_articles_data = url.read()
+#         get_articles_response = json.loads(get_articles_data)
+#
+#         articles_results = None
+#
+#         if get_articles_response['articles']:
+#             articles_results_list = get_articles_response['articles']
+#             articles_results = process_results(articles_results_list)
+#
+#     return articles_results
 
 
 def process_results(articles_list):
