2fc341c8e72e69681fe63ca3c460056ef1c6bea9
maxwellharon <maxwellharon@gmail.com>
2018-07-02 15:18:25 +0300

    class source error

--- app/models.py
+++ app/models.py
@@ -11,14 +11,14 @@ class Article:
         self.image_url = image_url
         self.publish_time = publish_time
 
-class Source:
-    """ Source class to define source objects"""
-
-    def __init__(self, id, name, description, url, category, country):
-
-        self.id = id
-        self.name = name
-        self.description = description
-        self.url = url
-        self.category = category
-        self.country = country
+# class Source:
+#     """ Source class to define source objects"""
+#
+#     def __init__(self, id, name, description, url, category, country):
+#
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.url = url
+#         self.category = category
+#         self.country = country
