ee7ba360efec5adce592934d6c5e3cc853b772e2
maxwellharon <maxwellharon@gmail.com>
2018-07-02 15:24:04 +0300

    Correct emmbedding of the API KEY and Base URL

--- app/requests.py
+++ app/requests.py
@@ -2,11 +2,11 @@ from app import app
 import urllib.request, json
 from .models import Article, Source
 
-# Getting API key
-# api_key = app.config['NEWS_API_KEY']
-#
-# # Getting the news base url
-# base_url = app.config["NEWS_API_BASE_URL"]
+Getting API key
+api_key = app.config['NEWS_API_KEY']
+
+# Getting the news base url
+base_url = app.config["NEWS_API_BASE_URL"]
 
 
 def get_articles(endpoint, category):
